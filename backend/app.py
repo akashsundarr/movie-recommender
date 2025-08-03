541626fb6d86eefa6eddd02db256a3e591f9ca73
Akash <akashsundarr@gmail.com>
2025-08-03 19:56:54 +0530

    fix: add to all routes

--- backend/app.py
+++ backend/app.py
@@ -19,10 +19,7 @@ with open(MODEL_PATH, "rb") as f:
     similarity, data = pickle.load(f)  # tuple unpacking
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": [
-    "https://movie-recommender-nu-three.vercel.app",
-    "https://movie-recommender-git-main-akash-sundars-projects.vercel.app"
-]}})
+CORS(app)
 
 
 
